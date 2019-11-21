8961897d3746957959da2fb466e798991cd42cab
Bhawna Kumari <kbhawna@thoughtworks.com>
2019-11-22 00:39:40 +0530

    Bhawna | First commit

---
+++ src/main/java/com/example/bank/com/example/controller/BankController.java
@@ -0,0 +1,16 @@
+package com.example.bank.com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BankController {
+
+    @GetMapping(path = "/home")
+    public ResponseEntity<Object> getHome(){
+        return new ResponseEntity<>("Hello Everyone", HttpStatus.OK);
+    }
+
+}
